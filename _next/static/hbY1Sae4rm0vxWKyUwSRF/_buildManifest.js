acfd04c5e9ced0c13c172663c62f33ec660f9299
Rabindra Kumar Parida <rudrapratap9@gmail.com>
2026-09-20 17:13:52 +0530

    deploy: publish 24/7 static build to GitHub Pages

---
+++ _next/static/hbY1Sae4rm0vxWKyUwSRF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b48c4a55f2770d2d.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
